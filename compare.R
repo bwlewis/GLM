125724792d219b487a292d375dfb87f2fa57f510
bwlewis <blewis@illposed.net>
2014-04-20 11:17:47 -0400

    Updated bootstrapped nnls comparison

--- compare.R
+++ compare.R
@@ -24,63 +24,48 @@ print(summary(MLE))
 
 # Let's compare the maximum likelihood estimate with a generic nonlinear
 # least squares solution...
-# Extract the response y and model matrix x from the MLE model object.
-x = MLE$x
-y = MLE$y
-logistic = function(x) 1/(1 + exp(-x))
-f = function(beta)
+nnls_glm = function(A,b,g)
 {
-   w  = x %*% beta
-   lw = logistic(w)
-   crossprod(y - lw)[1]
+  f = function(x) crossprod(b - g(A %*% x))[]
+  ans = optim(rep(0,ncol(A)), fn=f, method="BFGS")$par
+  names(ans) = colnames(A)
+  ans
 }
-NNLS = optim(rep(0,ncol(x)), fn=f, method="BFGS")
-names(NNLS$par) = colnames(x)
+
+# Extract the response y and model matrix x from the MLE model object.
+A = MLE$x    # model matrix
+b = MLE$y    # response vector
+NNLS = nnls_glm(A, b, binomial()$linkinv)
 
 # We can compare the coefficients estimated by each approach. They're
 # somewhat different.
-print(data.frame(MLE=coef(MLE), NNLS=NNLS$par))
+print(data.frame(MLE=coef(MLE), NNLS=NNLS))
 
 # Note that the residual norm will always be smaller for the NNLS estimate
 # because that's what that approach minimizes. Also note that the MLE approach
-# arrives at an estimate *much* faster.
-
+# arrives at an estimate much faster.
 
 # Let's compare bootstrapped estimates to get better a sense of how these two
-# estimation approaches vary...First, let's obtain bootstrapped estimates of
-# the standard MLE solution:
+# estimation approaches vary...
 set.seed(1)
-mleboot = replicate(1000,
+compare_bootstrap = replicate(1000,
 {
   i = sample(nobs,nobs,replace=TRUE)
-  glm.fit(x=x[i,],y=y[i], family=binomial())$coef
+  c(mle=glm.fit(x=A[i,],y=b[i], family=binomial())$coef,
+       nnls=nnls_glm(A[i,], b[i], binomial()$linkinv))
 })
-
-# And now with the nonlinear least squares solution:
-fboot = function(i)
-{
-  function(beta)
-  {
-    w  = x[i,] %*% beta
-    lw = logistic(w)
-    crossprod(y[i] - lw)[1]
-  }
-}
-nnlsboot = replicate(1000,
-{
-  i  = sample(nobs,nobs,replace=TRUE)
-  f = fboot(i)
-  optim(rep(0,ncol(x)), fn=f, method="BFGS")$par
-})
+# (The bootstrap results are returned in a matrix where each column represents
+# a resampled set of estimates. The first 7 rows contain the MLE estimates and
+# the next 7 the NNLS estimates.)
 
 # Let's compare histograms of one of the more significant model variables in
-# this problem, the square of age:
+# this problem, the square of age (in the 3rd and 10th rows):
 jpeg(file="compare.jpg",quality=100,width=800,height=800)
 split.screen(c(2,1))
-xrange = c(min(min(mleboot[3,]),min(nnlsboot[3,])),
-           max(max(mleboot[3,]),max(nnlsboot[3,])))
+xrange = c(min(min(compare_bootstrap[3,]),min(compare_bootstrap[10,])),
+           max(max(compare_bootstrap[3,]),max(compare_bootstrap[10,])))
 screen(1)
-hist(mleboot[3,],main="age^2 maximum likelihood estimate",col=4,xlim=xrange,breaks=25)
+hist(compare_bootstrap[3,],main="age^2 maximum likelihood estimate",col=4,xlim=xrange,breaks=25)
 screen(2)
-hist(nnlsboot[3,],main="age^2 direct nonlinear optimization",col=4,xlim=xrange,breaks=25)
+hist(compare_bootstrap[10,],main="age^2 direct nonlinear optimization",col=4,xlim=xrange,breaks=25)
 dev.off()
