f17619ee50397c4dc07477a6a703074b52e39628
bwlewis <blewis@illposed.net>
2014-04-30 13:20:14 -0400

    Added qr newton implementation and better comparison code

--- compare.R
+++ compare.R
@@ -1,46 +1,11 @@
-# Generalized Linear Model Experiments
-#
-# Compare a logistic regression computed using the usual maximum likelihood
-# estimate with an alternative model computed directly by a nonlinear least
-# squares estimate.
-
-# We use the 'Contraception' data set from the mlmRev package in favor of a
-# made-up example. You might need to install the mlmRev package to obtain
-# these data. We mostly follow the very concise introduction to generalized
-# linear models by Doug Bates available here:
-# http://www.stat.wisc.edu/courses/st849-bates/lectures/GLMH.pdf
+source("implementations.R")
 data("Contraception",package="mlmRev")
-Contraception$use = as.numeric(Contraception$use) - 1
-nobs = nrow(Contraception)
+# Model estimated with R's glm function, returning model matrix and response
+# in $x and $y, respectively:
+R_GLM = glm(formula = use ~ age + I(age^2) + urban + livch, family = binomial, x=TRUE, data=Contraception)
+# Model estimated with our radically stripped-down minimalist implementation:
+mini = irls(R_GLM$x, R_GLM$y, family=binomial)
+print(data.frame(R_GLM=coef(R_GLM), minimalist=coef(mini)))
 
-# Compute the usual maximum likeliehood estimate. We use a formula suggested
-# by Doug Bates in the note cited above, worth reading! Note that we also
-# specify x=TRUE, which builds and returns a model matrix for us. We'll use
-# that later.
-BIN = glm(formula = use ~ age + I(age^2) + urban + livch,
-          family = binomial(), x=TRUE, data=Contraception)
-
-GAU = glm(formula = use ~ age + I(age^2) + urban + livch, start=coef(l),
-          family = gaussian(link="logit"), x=TRUE, data=Contraception)
-
-# Here is the standard model summary provided by GLM:
-print(summary(BIN))
-
-# Let's compare the maximum likelihood estimate with a generic nonlinear
-# least squares solution...
-nnls_glm = function(A,b,g)
-{
-  f = function(x) crossprod(b - g(A %*% x))[]
-  ans = optim(rep(0,ncol(A)), fn=f, method="BFGS")$par
-  names(ans) = colnames(A)
-  ans
-}
-
-# Extract the response y and model matrix x from the MLE model object.
-A = BIN$x    # model matrix
-b = BIN$y    # response vector
-NNLS = nnls_glm(A, b, binomial()$linkinv)
-
-# We can compare the coefficients estimated by each approach. They're
-# somewhat different.
-print(data.frame(BIN=coef(BIN),GAU=coef(GAU),NNLS=NNLS))
+iqrn = irls_qrnewton(R_GLM$x, R_GLM$y, family=binomial)
+print(data.frame(R_GLM=coef(R_GLM), qr_newton=coef(iqrn)))
---
+++ implementations.R
@@ -0,0 +1,44 @@
+irls =
+function(A, b, family=binomial, maxit=25, tol=1e-08)
+{
+  x = rep(0,ncol(A))
+  for(j in 1:maxit)
+  {
+    eta   = A %*% x
+    g     = family()$linkinv(eta)
+    gprime = family()$mu.eta(eta)
+    z     = eta + (b - g) / gprime
+    W     = as.vector(gprime^2 / family()$variance(g))
+    xold  = x
+    x     = solve(crossprod(A,W*A), crossprod(A,W*z), tol=2*.Machine$double.eps)
+    if(sqrt(crossprod(x-xold)) < tol) break
+  }
+  list(coefficients=x,iterations=j)
+}
+
+irls_qrnewton =
+function(A, b, family=binomial, maxit=25, tol=1e-08)
+{
+  r  = b
+  QR = qr(A)
+  Q  = qr.Q(QR)
+  R  = qr.R(QR)
+  for(j in 1:maxit)
+  {
+    eta    = b - r
+    g      = family()$linkinv(eta)
+    gprime = family()$mu.eta(eta)
+    z      = eta + (b - g) / gprime
+    W      = as.vector(gprime^2 / family()$variance(g))
+    wmin   = min(W)
+    if(wmin < sqrt(.Machine$double.eps)) warning("Tiny weights encountered, likely numerical problems!")
+    rold   = r
+    C   = chol(crossprod(Q, W*Q))
+    s   = forwardsolve(t(C), crossprod(Q,W*z))
+    s   = backsolve(C,s)
+    r      = b - Q %*% s
+    if(sqrt(crossprod(r-rold)) < tol) break
+  }
+  x = backsolve(R, crossprod(Q,b-r))
+  list(coefficients=x,iterations=j)
+}
